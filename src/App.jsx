4626282dfad474c5d83c2e2c58755302608c409d
damitha sahan <sahanchandrasena462@gmail.com>
2024-06-18 23:18:45 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const  App = () => {
+  return (
+    <h1 className="text-3xl font-bold underline bg-slate-400">
+      Hello world!
+    </h1>
+  )
+}
+
+export default App
